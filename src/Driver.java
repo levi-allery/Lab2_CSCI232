1e929ed58413e1d8a2fa19ad9535de581aea3fff
levi-allery <ljallery@gmail.com>
2016-10-05 15:54:11 -0600

    "Fully working program with menu"

--- src/Driver.java
+++ src/Driver.java
@@ -17,33 +17,51 @@ public class Driver {
     }
 
     public void menu() {
-        HashTable h = new HashTable(10);
-        h.insert(10);
-        h.insert(2);
-        h.insert(20);
-        h.insert(32);
-        h.show();
-        h.find(431);
+        //Variables
+        Scanner scn = new Scanner(System.in);
+        int arraySize = 0;
+        int items;
+        boolean done = false;
+        boolean tableCreated = false;
 
-//
-//        Scanner scn = new Scanner(System.in);
-//        int arraySize = 0;
-//        int items = 0;
-//        System.out.println("Enter size of has table: ");
-//        arraySize = scn.nextInt();
-//        System.out.println("Enter initial number of item: ");
-//        items = scn.nextInt();
-//        System.out.println("Enter first letter of show, insert, or find: ");
-//        String selection = scn.next();
-//        switch (selection) {
-//            case s://Shows the table along with binary tree in array location
-//                break;
-//            case i://Inserts into hash table
-//                break;
-//            case f://Finds an int within the table
-//                break;
-//            default:
-//                System.out.println("Please enter either 's', 'i', or 'f' \n");
-//        }
+        while(!tableCreated) {
+            System.out.println("Enter size of has table: ");
+            arraySize = scn.nextInt();
+            if (arraySize <= 0) {
+                System.out.println("Please enter something greater then zero");
+            }
+            else{
+                tableCreated = true;
+            }
+        }
+        HashTable h = new HashTable(arraySize);
+        System.out.println("Enter initial number of items: ");
+        items = scn.nextInt();
+        for (int i = 0; i < items; i++) {
+            h.insert(i*i^2);
+        }
+        while (!done) {
+            System.out.println("Enter first letter of show, insert, find, or exit: ");
+            String selection = scn.next();
+            switch (selection) {
+                case "s"://Shows the table along with binary tree in array location
+                    h.show();
+                    break;
+                case "i"://Inserts into hash table
+                    System.out.println("Enter key value to insert: ");
+                    h.insert(scn.nextInt());
+                    break;
+                case "f"://Finds an int within the table
+                    System.out.println("Enter key value to find: ");
+                    h.find(scn.nextInt());
+                    break;
+                case "e": //Exits program
+                    done = true;
+                    break;
+                default:
+                    System.out.println("Please enter either 's', 'i', or 'f' \n");
+            }
+        }
     }
 }
+
